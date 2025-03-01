45ceb87d761148cc5e0b550cd388b1d320e6ab58
ashishjeswani <ashish.jeswani@noovosoft.com>
2025-03-01 14:35:01 +0530

    CK | move from pupeetteer to rapidapi

--- utils/reel-downloader.js
+++ utils/reel-downloader.js
@@ -1,97 +1,26 @@
-import chromeAWS from "chrome-aws-lambda";
-import puppeteerCore from "puppeteer-core";
-import puppeteerDev  from "puppeteer";
-
-let chrome = {};
-let puppeteer;
-
-if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-  chrome = chromeAWS
-  puppeteer = puppeteerCore
-} else {
-  puppeteer = puppeteerDev
-}
 
 export async function downloadReel(reelUrl) {
+  const url = `${process.env.RAPID_API_URL}?url=${reelUrl}`;
 
-  let options = {};
-
-  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-    options = {
-      args: [
-              ...chrome.args,
-             "--hide-scrollbars",
-             "--disable-web-security",
-             '--no-sandbox',
-             '--disable-setuid-sandbox',
-             '--disable-dev-shm-usage',
-             '--disable-accelerated-2d-canvas',
-             '--disable-gpu',
-             '--disable-extensions',
-             '--disable-infobars',
-      ],
-      defaultViewport: chrome.defaultViewport,
-      executablePath: await chrome.executablePath,
-      headless: true,
-      ignoreHTTPSErrors: true,
-    };
-  }
-  else {
-    options = {
-      args: [
-        "--hide-scrollbars",
-        "--disable-web-security",
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-dev-shm-usage',
-        '--disable-accelerated-2d-canvas',
-        '--disable-gpu',
-        '--disable-extensions',
-        '--disable-infobars',
-      ],
-        defaultViewport: null,
-        headless: true,
-        ignoreHTTPSErrors: true,
-    };
-  }
+  const options = {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-key': process.env.RAPID_API_KEY,
+      'x-rapidapi-host': process.env.RAPID_API_HOST
+    }
+  };
 
   try {
-
-    let browser = await puppeteer.launch(options);
-
-    const page = await browser.newPage();
-
-    await page.goto('https://indown.io/reels');
-
-    await new Promise(resolve => setTimeout(resolve, 3000));
-
-    const reelLinkInputSelector = '#link'
-    await page.waitForSelector(reelLinkInputSelector);
-
-
-    await page.evaluate((reelUrl, reelLinkInputSelector) => {
-      const reelLinkInput = document.querySelector(reelLinkInputSelector);
-      reelLinkInput.value = reelUrl;
-    }, reelUrl, reelLinkInputSelector);
-
-    const searchButtonSelector = '#get'
-    const searchReelButton = await page.waitForSelector(searchButtonSelector);
-    await searchReelButton.click();
-
-    await new Promise(resolve => setTimeout(resolve, 10000));
-
-    let reelDownloadLink;
-
-    const downloadButtonSelector = '#result > div > div > div.mt-3 > div > a.mb-2'
-    reelDownloadLink = await page.evaluate((downloadButtonSelector) => {
-      return document.querySelector(downloadButtonSelector).getAttribute('href');
-    }, downloadButtonSelector);
-
-    return reelDownloadLink;
-
-  }
-  catch (error) {
-    console.error("ERROR occurred while downloading reel", error);
+    const response = await fetch(url, options);
+    const result = await response.json();
+    if (result.links.length > 0) {
+      return result.links[1].link;
+    }
     return null;
   }
+  catch (error) {
+    console.error(error);
+    return null;
+  }
+
 }
